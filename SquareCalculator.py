6fec63de151f147c6c9c0a6971d08ca332d1bc4f
paroffbrandon <74071720+paroffbrandon@users.noreply.github.com>
2021-08-23 17:41:03 -0400

    Add files via upload

---
+++ SquareCalculator.py
@@ -0,0 +1,38 @@
+# This is the Square Calculator for the "Everything Calculator" Project
+# Math package
+import math
+
+def findArea(l):
+    return l * l
+
+def findPerimeter(l):
+    return l + l + l + l
+
+def findLength(A):
+    return math.sqrt(l)
+
+def findLength(P):
+    return p / 4
+
+print("  Welcome to the Square Caclulator!")
+print("If you do not know the answer to the value, please answer 0")
+
+while True:
+    l = input("What is the Length of the sides of the square? ")
+    p = input("What is the Perimeter of the of the square? ")
+    a = input("What is the Area of the of the square? ")
+
+    if ((a == 0 and p == 0) or (a == 0) or (p == 0)):
+        print("Your Length is ", l)
+        print("Your Area is ", findArea(l))
+        print("Your Perimeter is ", findPerimeter(l))
+
+    elif (l == 0 and a == 0):
+        print("Your Length is ", findlegnth(P))
+        print("Your Area is ")
+
+    elif (l == 0 and p == 0):
+        print("Y")
+
+    else:
+        break
