45655ee671ab91c670a3b6d0d3a4d15941ff0a07
paroffbrandon <74071720+paroffbrandon@users.noreply.github.com>
2021-08-24 14:10:34 -0400

    Changed up findLengthA and findLengthP
    I still do not have these acutally working but the functions the need the Length work fine. I have not yet figured out how to update inputs
    an example is if I have findLengthP and get the length from the perimeter, I cannot find a way to update the length so I can find the area
    If I cannot find a way to do that then I can create a function to find both the Area and the Length from the Perimeter

--- SquareCalculator.py
+++ SquareCalculator.py
@@ -10,13 +10,13 @@ def findPerimeter(l):
     l = float(l)
     return l * 4
 
-def findLength(A):
-    A = float(A)
-    return math.sqrt(A)
+def findLengthP(p):
+    p = float(p)
+    return p / 4
 
-def findLength(P):
-    P = float(P)
-    return A / 4
+def findLengthA(a):
+    a = float(a)
+    return math.sqrt(a)
 
 print("    Welcome to the Square Caclulator!")
 print("If you do not know the answer to the value, please answer 0")
@@ -24,8 +24,8 @@ print("     ")
 
 while True:
     l = input("What is the Length of the sides of the square? ")
-    p = input("What is the Perimeter of the of the square? ")
     a = input("What is the Area of the of the square? ")
+    p = input("What is the Perimeter of the of the square? ")
 
     if ((a == 0 and p == 0) or (a == 0) or (p == 0)):
         print("Your Length is " + str(l))
@@ -35,6 +35,7 @@ while True:
     elif (l == 0 and a == 0):
         print("Your Length is "+ str(findlegnth(P)))
         print("Your Area is " + str(findArea(l)))
+        print("Your Perimeter " + str((p))
 
     elif (l == 0 and p == 0):
         print("Y")
